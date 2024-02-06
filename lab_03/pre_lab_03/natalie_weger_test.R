2cf0fb52a3d3cec51a534191668d486c1e3c078b
nweger8 <nweger@terpmail.umd.edu>
2024-02-06 16:54:15 -0500

    test

--- lab_03/pre_lab_03/natalie_weger_test.R
+++ lab_03/pre_lab_03/natalie_weger_test.R
@@ -1,1 +1,12 @@
-File from Wells 
+#Test file from Wells 
+
+install.packages("tidyverse")
+yes
+library(tidyverse)
+install.packages("janitor")
+install.packages("rmarkdown")
+install.packages("skimr")
+install.packages("swirl")
+install.packages("rio")
+Yes
+
